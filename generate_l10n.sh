42cca4bb1abb0e3298eb20fe16189f11ea65072f
pood1e <codingpood1e@gmail.com>
2025-06-23 21:03:39 +0800

    add settings l10n

--- generate_l10n.sh
+++ generate_l10n.sh
@@ -11,4 +11,9 @@ flutter gen-l10n --arb-dir lib/modules/dashboard/l10n \
   --output-localization-file dashboard_localizations.dart \
   --output-class DashboardLocalizations
 
+flutter gen-l10n --arb-dir lib/modules/settings/l10n \
+  --template-arb-file intl_zh.arb \
+  --output-localization-file settings_localizations.dart \
+  --output-class SettingsLocalizations
+
 echo "generate l10n successfully!"
