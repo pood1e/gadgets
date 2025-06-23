3e573d88ffc43cf1530df1f5bdd6b90e7d9f5a9c
pood1e <codingpood1e@gmail.com>
2025-06-23 22:45:09 +0800

    fix settings l10n🐛

--- lib/modules/settings/ui/settings.dart
+++ lib/modules/settings/ui/settings.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/widgets.dart';
+import 'package:gadgets/modules/settings/l10n/settings_localizations.dart';
+import 'package:gadgets/shared/view_models/appbar_view_model.dart';
+import 'package:provider/provider.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   const SettingsView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SettingViewState();
+}
+
+class _SettingViewState extends State<SettingsView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AppbarViewModel>().changeConfig(
+        AppBarConfig(
+          id: 'settings',
+          title: SettingsLocalizations.of(context)!.title,
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) => const Center(child: Text('settings'));
--- test/modules/settings/ui/settings_test.dart
+++ test/modules/settings/ui/settings_test.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gadgets/modules/settings/l10n/settings_localizations.dart';
 import 'package:gadgets/modules/settings/ui/settings.dart';
+import 'package:gadgets/shared/l10n/app_localizations.dart';
+import 'package:gadgets/shared/ui/responsive_scaffold.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
+  createTestWidget() => MaterialApp.router(
+    localizationsDelegates: const [
+      AppLocalizations.delegate,
+      SettingsLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+    supportedLocales: const [Locale('en'), Locale('zh')],
+    locale: const Locale('zh'),
+    routerConfig: GoRouter(
+      initialLocation: '/settings',
+      routes: [
+        ShellRoute(
+          builder: (context, state, child) => ResponsiveScaffold(child: child),
+          routes: [
+            GoRoute(path: '/settings', builder: (_, _) => const SettingsView()),
+          ],
+        ),
+      ],
+    ),
+  );
+
   group('Settings', () {
     testWidgets('should show default text', (tester) async {
       // Arrange
       // Act
-      await tester.pumpWidget(const MaterialApp(home: SettingsView()));
+      await tester.pumpWidget(createTestWidget());
+      await tester.pumpAndSettle();
 
       // Assert
       expect(find.text('settings'), findsOneWidget);
+      expect(find.text('设置'), findsOneWidget);
     });
   });
 }
