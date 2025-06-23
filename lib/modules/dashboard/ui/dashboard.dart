b4286e6a2a6b807cf527674f872831edd03a9e5a
pood1e <codingpood1e@gmail.com>
2025-06-23 22:41:03 +0800

    fix dashboard l10n🐛

--- lib/modules/dashboard/ui/dashboard.dart
+++ lib/modules/dashboard/ui/dashboard.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:gadgets/modules/dashboard/l10n/dashboard_localizations.dart';
+import 'package:gadgets/shared/view_models/appbar_view_model.dart';
+import 'package:provider/provider.dart';
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AppbarViewModel>().changeConfig(
+        AppBarConfig(
+          id: 'dashboard',
+          title: DashboardLocalizations.of(context)!.title,
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) => const Center(child: Text('dashboard'));
--- test/modules/dashboard/ui/dashboard_test.dart
+++ test/modules/dashboard/ui/dashboard_test.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gadgets/modules/dashboard/l10n/dashboard_localizations.dart';
 import 'package:gadgets/modules/dashboard/ui/dashboard.dart';
+import 'package:gadgets/shared/l10n/app_localizations.dart';
+import 'package:gadgets/shared/ui/responsive_scaffold.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
+  createTestWidget() => MaterialApp.router(
+    localizationsDelegates: const [
+      AppLocalizations.delegate,
+      DashboardLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+    ],
+    supportedLocales: const [Locale('en'), Locale('zh')],
+    locale: const Locale('zh'),
+    routerConfig: GoRouter(
+      initialLocation: '/',
+      routes: [
+        ShellRoute(
+          builder: (context, state, child) => ResponsiveScaffold(child: child),
+          routes: [
+            GoRoute(path: '/', builder: (_, _) => const DashboardView()),
+          ],
+        ),
+      ],
+    ),
+  );
+
   group('Dashboard', () {
     testWidgets('should show default text', (tester) async {
       // Arrange
       // Act
-      await tester.pumpWidget(const MaterialApp(home: DashboardView()));
+      await tester.pumpWidget(createTestWidget());
+      await tester.pumpAndSettle();
 
       // Assert
       expect(find.text('dashboard'), findsOneWidget);
+      expect(find.text('仪表盘'), findsOneWidget);
     });
   });
 }
